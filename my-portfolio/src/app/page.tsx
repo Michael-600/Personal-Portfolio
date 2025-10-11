5a58e247f29cf0d7fd082371fd3ae72bfba9f49b
Michael Hayford <mqh6@duke.edu>
2025-10-11 18:05:07 -0400

    Bootstrap the app

---
+++ my-portfolio/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen grid place-items-center">
+      <h1 className="text-4xl font-bold text-yellow-400">
+        Tailwind v4 is working ✨
+      </h1>
+    </main>
+  );
+}
